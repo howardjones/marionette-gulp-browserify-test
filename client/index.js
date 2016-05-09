59d23e5b69245271387f6b45d9f0a96dcfff6c96
howardjones <howie@thingy.com>
2016-05-09 09:50:06 +0100

    Added bootstrap/bootswatch using LESS. Marionette still doesn't load.

--- client/index.js
+++ client/index.js
@@ -1,10 +1,25 @@
 var $ = require('jquery');
+var Backbone = require('backbone');
+Backbone.$ = $;
+var Marionette = require('backbone.marionette');
+
+console.log('hello2');
+
+var HJApp = Marionette.Application.extend({
+    initialize: function(options) {
+        console.log('Initialising');
+    }
+});
+
+var myApp = new HJApp();
+console.dir(myApp);
+myApp.start();
 
 console.log('hello2');
 
 $(document).ready(function () {
 
-    $('body').append('<h3>Boink</h3>');
-    $('body').append('<h4>Boink</h4>');
+    $('body').append('<h3>Zort</h3>');
+    $('body').append('<h4>Narf</h4>');
 
 });
